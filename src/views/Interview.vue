25831cc0fb02037e52e5f403c68e67e5c206048e
Tim Busuttil <timothyjbusuttil@gmail.com>
2018-10-02 13:15:43 +1000

    add video container

--- src/views/Interview.vue
+++ src/views/Interview.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="interview">
     <h1>interview video</h1>
-    <iframe class="video" width="800" height="450"
-      src="https://www.youtube.com/embed/tgbNymZ7vqY">
-    </iframe>
+    <div class="videoContainer">
+      <iframe class="video" width="800" height="450"
+        src="https://www.youtube.com/embed/tgbNymZ7vqY">
+      </iframe>
+    </div>
   </div>
 </template>
 
 <style>
+.videoContainer {
+  background: black;
+  width: 800px;
+}
+
 .video {
   border: none;
 }
