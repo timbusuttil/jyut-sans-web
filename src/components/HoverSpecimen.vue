2b59341265cfcbb2f280f70a0057c3a29c53ceb5
Tim Busuttil <timothyjbusuttil@gmail.com>
2018-10-02 12:02:32 +1000

    change rising/falling/stable classes on specimen prototype

--- src/components/HoverSpecimen.vue
+++ src/components/HoverSpecimen.vue
@@ -7,35 +7,35 @@
       <div class="character falling">d</div>
       <div class="character stable">e</div>
       <div class="character stable">f</div>
-      <div class="character">g</div>
-      <div class="character">h</div>
+      <div class="character stable">g</div>
+      <div class="character stable">h</div>
       <div class="character">i</div>
-      <div class="character">j</div>
+      <div class="character falling">j</div>
       <div class="character">k</div>
-      <div class="character">l</div>
+      <div class="character falling">l</div>
       <div class="character">m</div>
       <div class="character">n</div>
-      <div class="character">o</div>
+      <div class="character rising">o</div>
       <div class="character">p</div>
-      <div class="character">q</div>
+      <div class="character stable">q</div>
       <div class="character">r</div>
       <div class="character">s</div>
-      <div class="character">t</div>
-      <div class="character">u</div>
+      <div class="character stable">t</div>
+      <div class="character rising">u</div>
       <div class="character">v</div>
       <div class="character">w</div>
       <div class="character">x</div>
       <div class="character">y</div>
-      <div class="character">z</div>
-      <div class="character">!</div>
+      <div class="character falling">z</div>
+      <div class="character rising">!</div>
       <div class="character">?</div>
       <div class="character">.</div>
       <div class="character">,</div>
     </div>
     <div class="hoverToggles">
-      <div class="risingToggle" @mouseover="focusCharacters('rising')" @mouseleave="focusCharacters('all')"></div>
-      <div class="fallingToggle" @mouseover="focusCharacters('falling')" @mouseleave="focusCharacters('all')"></div>
-      <div class="stableToggle" @mouseover="focusCharacters('stable')" @mouseleave="focusCharacters('all')"></div>
+      <div class="rising toggle" @mouseover="focusCharacters('rising')" @mouseleave="focusCharacters('all')"></div>
+      <div class="falling toggle" @mouseover="focusCharacters('falling')" @mouseleave="focusCharacters('all')"></div>
+      <div class="stable toggle" @mouseover="focusCharacters('stable')" @mouseleave="focusCharacters('all')"></div>
     </div>
   </div>
 </template>
@@ -80,24 +80,9 @@ export default {
   z-index: 2;
 }
 
-.risingToggle {
+.toggle {
   width: 80px;
   height: 80px;
-  background: pink;
-  margin: 20px 20px 0 0;
-}
-
-.fallingToggle {
-  width: 80px;
-  height: 80px;
-  background: gold;
-  margin: 20px 20px 0 0;
-}
-
-.stableToggle {
-  width: 80px;
-  height: 80px;
-  background: tomato;
   margin: 20px 20px 0 0;
 }
 
