700496b6ab452f0fdc64ac87f33c6d0b6b4270c0
Tim Busuttil <timothyjbusuttil@gmail.com>
2018-10-03 00:14:03 +1000

    create audioSpecimen

---
+++ src/components/AudioSpecimen.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3>click on the numbers to hear the tones</h3>
+    <button type="button" name="button" @click="playSound">1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AudioSpecimen',
+  data () {
+    return {
+      // audio: new Audio('@/src/assets/1 do.mp3')
+    }
+  },
+  methods: {
+    // playSound () {
+    //   this.audio.play()
+    // }
+  }
+}
+</script>
+
+<style>
+
+</style>
