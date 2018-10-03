fb8107ed63df120bcef44a3ab059b20637e2142a
Tim Busuttil <timothyjbusuttil@gmail.com>
2018-10-03 11:41:04 +1000

    add how to play content

--- src/views/HowToPlay.vue
+++ src/views/HowToPlay.vue
@@ -1,6 +1,60 @@
 <template>
   <div class="howToPlay">
     <h1>how to play</h1>
+    <div class="flexContainer">
+      <div v-for="(item, index) in flexItems" class="flexItem" :key="index">
+        <img class="featureImage" :src="item.image" :alt="item.alt">
+        <p>{{item.text}}</p>
+      </div>
+    </div>
     <router-link to="/interactive">play</router-link>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'howToPlay',
+  data () {
+    return {
+      flexItems: [
+        {
+          image: require('../assets/typeface/eye.png'),
+          alt: '',
+          text: 'Interact with words by clicking on them'
+        },
+        {
+          image: require('../assets/typeface/eye.png'),
+          alt: '',
+          text: 'Click on the tone levels to see how tone changes meaning'
+        },
+        {
+          image: require('../assets/typeface/eye.png'),
+          alt: '',
+          text: 'Turn the grid on and off at any time'
+        }
+      ]
+    }
+  }
+}
+
+</script>
+
+<style>
+.flexContainer {
+  display: flex;
+  width: 100%;
+  height: 200px;
+  border: 1px solid red;
+}
+
+.flexItem {
+  flex: 1;
+  border: 1px solid blue;
+}
+
+.featureImage {
+  border: 3px solid black;
+  border-radius: 100%;
+  object-fit: contain;
+}
+</style>
