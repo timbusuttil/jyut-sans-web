b0009d8071829b45026ed15d78d791fb27da28c9
Tim Busuttil <timothyjbusuttil@gmail.com>
2018-10-17 14:17:27 +1100

    vimeo embed removed title

--- src/views/Interview.vue
+++ src/views/Interview.vue
@@ -8,7 +8,7 @@
         src="https://www.youtube.com/embed/tgbNymZ7vqY">
       </iframe> -->
       <p style="position: absolute; padding: 100px;">loading</p>
-      <iframe src="https://player.vimeo.com/video/294314377?color=ffffff&byline=0&portrait=0&title=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+      <iframe src="https://player.vimeo.com/video/294314377?color=ffffff&byline=0&portrait=0&title=0&callback=videoLoaded" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
     </div>
   </div>
 </template>
